ea79e5ca89bce9c6fc3b8cdf20a81e6db044f649
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-11-21 14:36:59 -0500

    models-2.0

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -25,3 +25,28 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
+// Index for faster lookups by email
+userSchema.index({ email: 1 }, { unique: true });
+
+// Hash password before saving to the database
+userSchema.pre('save', async function(next) {
+  try {
+    if (this.isModified('password')) {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+
+module.exports = User;
+//end
+
+
