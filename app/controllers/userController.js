4093ccd45aa27114553e59f7f98cfe52a2fbf788
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-11-21 14:29:20 -0500

    controller -1

---
+++ app/controllers/userController.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs/dist/bcrypt")
+const User = require("../models/userModel")
+const bcryptjs = require("bcryptjs")
+const { validationResult } = require('express-validator');
+const userService = require("../services/userService")
+async function RegisterUser(request, response){
+    const {username, email, password} = request.body
+    console.log(request.body)
+    try{
+        const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+        const user = await userService.findOneUser(email)
+        if (user){
+            return response.status(400).json({message: "user already exist"})  
+        }
+        const createdUser = await userService.createUser({username, email, password})
+        return response.status(201).json({message: "user created successfully",user:createdUser})
+    }
+
+    catch(error){
+        return response.status(500).json({message: error.message})
+    }
+}
+
