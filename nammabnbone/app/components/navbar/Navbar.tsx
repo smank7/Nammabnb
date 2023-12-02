51a16b5ba5b9b3d3e84c814a7daeea1308b5c3c2
Deepak Sundar Narayanan <narayanan.dee@northeastern.edu>
2023-12-02 00:49:48 -0500

    Host_listing_UI
    Completed the navbar, the host listing and the filter below the navbar(snow, mountain, beach etc)

---
+++ nammabnbone/app/components/navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import Categories from "./Categories";
+import Container from "../Container";
+import Logo from "./Logo";
+
+const Navbar = () => {
+    return( 
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+      <Container>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+        </div>
+      </Container>
+      </div>
+    <Categories />
+  </div>
+    );
+
+}
+
+export default Navbar;
