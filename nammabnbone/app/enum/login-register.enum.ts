a3fb2be7d3042716cd395953ca95e18d4a0cdb2a
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-12-12 08:59:09 -0500

    Add enums for static text

---
+++ nammabnbone/app/enum/login-register.enum.ts
@@ -0,0 +1,20 @@
+export enum AccountField {
+	EMAIL = 'email',
+	PASSWORD = 'password',
+	NAME = 'name',
+}
+
+export enum RegisterModalContentField {
+	TITLE = 'Welcome back',
+	SUBTITLE = 'Login to your account!',
+}
+
+export enum AccountProviderLabel {
+	GOOGLE = 'Continue with Google',
+	GITHUB = 'Continue with Github',
+}
+
+export enum AccountProvider {
+	GOOGLE = 'google',
+	GITHUB = 'github',
+}
