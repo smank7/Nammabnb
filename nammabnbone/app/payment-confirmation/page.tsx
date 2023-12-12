d9015fbcc5004d60be3bd89c1f73c29310204666
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-12-12 01:26:02 -0500

    Implement payment confirmation page

---
+++ nammabnbone/app/payment-confirmation/page.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { TickIcon } from '@/app/assets/tick-icon';
+import Button from '@/app/components/Button';
+import Loader from '@/app/components/Loader';
+import axios from 'axios';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+
+const PaymentConfirmation = () => {
+	const router = useRouter();
+	const queryParams = useSearchParams();
+	const startDate = queryParams?.get('startDate') || '';
+	const endDate = queryParams?.get('endDate') || '';
+	const listingId = queryParams?.get('listingId') || '';
+	const price = queryParams?.get('totalPrice') || '';
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		if (!startDate || !endDate || !listingId || !price) {
+			return;
+		}
+
+		setIsLoading(true);
+		const totalPrice = Number(price);
+		axios
+			.post('/api/reservations', {
+				totalPrice,
+				startDate,
+				endDate,
+				listingId,
+			})
+			.then(() => {
+				toast.success('Listing reserved!');
+			})
+			.catch(() => {
+				toast.error('Something went wrong.');
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	}, [
+		price,
+		startDate,
+		endDate,
+		listingId,
+		queryParams,
+		router,
+		setIsLoading,
+	]);
+
+	const redirectToReservationPage = () => {
+		router.push('/trips');
+	};
+
+	if (isLoading) {
+		return (
+			<div className='flex justify-center flex-col items-center'>
+				<Loader />
+				<p className='text-lg'>
+					Please wait while we are processing your payment.
+				</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex justify-center flex-col gap-6 items-center'>
+			<TickIcon />
+			<div className='text-center'>
+				<h1 className='text-lg font-semibold mb-2'>
+					Thanks you for the payment.
+				</h1>
+				<p className='text-md'>
+					Your trip has been successfully booked.
+				</p>
+			</div>
+			<div className='w-40'>
+				<Button
+					label='Manage trips'
+					onClick={redirectToReservationPage}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default PaymentConfirmation;
