1eeb7ce01e466846b0932063b4c8235bbf2c9a7e
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-12-13 09:37:09 -0500

    Remove prop drilling from properties components

--- nammabnbone/app/properties/PropertiesClient.tsx
+++ nammabnbone/app/properties/PropertiesClient.tsx
@@ -1,53 +1,49 @@
 'use client';
 
-import { toast } from "react-hot-toast";
-import axios from "axios";
-import { useCallback, useState } from "react";
-import { useRouter } from "next/navigation";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
+import { toast } from 'react-hot-toast';
 
-import { SafeListing, SafeUser } from "@/app/types";
+import { SafeListing } from '@/app/types';
 
-import Heading from "@/app/components/Heading";
-import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
+import Container from '@/app/components/Container';
+import Heading from '@/app/components/Heading';
+import ListingCard from '@/app/components/listings/ListingCard';
 
 interface PropertiesClientProps {
-  listings: SafeListing[],
-  currentUser?: SafeUser | null,
+	listings: SafeListing[];
 }
 
-const PropertiesClient: React.FC<PropertiesClientProps> = ({
-  listings,
-  currentUser
-}) => {
-  const router = useRouter();
-  const [deletingId, setDeletingId] = useState('');
+const PropertiesClient: React.FC<PropertiesClientProps> = ({ listings }) => {
+	const router = useRouter();
+	const [deletingId, setDeletingId] = useState('');
 
-  const onDelete = useCallback((id: string) => {
-    setDeletingId(id);
+	const onDelete = useCallback(
+		(id: string) => {
+			setDeletingId(id);
 
-    axios.delete(`/api/listings/${id}`)
-    .then(() => {
-      toast.success('Listing deleted');
-      router.refresh();
-    })
-    .catch((error) => {
-      toast.error(error?.response?.data?.error)
-    })
-    .finally(() => {
-      setDeletingId('');
-    })
-  }, [router]);
+			axios
+				.delete(`/api/listings/${id}`)
+				.then(() => {
+					toast.success('Listing deleted');
+					router.refresh();
+				})
+				.catch((error) => {
+					toast.error(error?.response?.data?.error);
+				})
+				.finally(() => {
+					setDeletingId('');
+				});
+		},
+		[router]
+	);
 
-
-  return ( 
-    <Container>
-      <Heading
-        title="Properties"
-        subtitle="List of your properties"
-      />
-      <div 
-        className="
+	return (
+		<Container>
+			<Heading title='Properties' subtitle='List of your properties' />
+			<div
+				className='
           mt-10
           grid 
           grid-cols-1 
@@ -57,22 +53,20 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
           xl:grid-cols-5
           2xl:grid-cols-6
           gap-8
-        "
-      >
-        {listings.map((listing: any) => (
-          <ListingCard
-            key={listing.id}
-            data={listing}
-            actionId={listing.id}
-            onAction={onDelete}
-            disabled={deletingId === listing.id}
-            actionLabel="Delete property"
-            currentUser={currentUser}
-          />
-        ))}
-      </div>
-    </Container>
-   );
-}
- 
+        '>
+				{listings.map((listing: any) => (
+					<ListingCard
+						key={listing.id}
+						data={listing}
+						actionId={listing.id}
+						onAction={onDelete}
+						disabled={deletingId === listing.id}
+						actionLabel='Delete property'
+					/>
+				))}
+			</div>
+		</Container>
+	);
+};
+
 export default PropertiesClient;
